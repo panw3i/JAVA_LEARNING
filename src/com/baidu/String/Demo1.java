7bef7e365128a611303ecf92128e4cb7446f0fce
panw3i <panw3i@gmail.com>
2017-01-31 22:11:46 +0800

    String类

--- src/com/baidu/String/Demo1.java
+++ src/com/baidu/String/Demo1.java
@@ -7,6 +7,7 @@ public class Demo1 {
     public static void main(String[] args) {
 
         String str = "abc";  //"abc"可以看成是一个字符串对象
+
         str = "def";  // 等于 断开了str指向"abc",重新指向"def","abc"成为垃圾
 
 
