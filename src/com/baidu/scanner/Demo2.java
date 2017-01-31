0dd4bdb9b20c9fda6a1f38237136eae3ee7e2662
panw3i <panw3i@gmail.com>
2017-01-31 22:04:16 +0800

    String nextLine()

---
+++ src/com/baidu/scanner/Demo2.java
@@ -0,0 +1,40 @@
+package com.baidu.scanner;
+
+import java.util.Scanner;
+
+/**
+ * Created by pan on 2017/1/31.
+ */
+public class Demo2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+//        System.out.println("请输入第一个整数");
+//        int i = sc.nextInt();
+//        System.out.println(i);
+//        System.out.println("请输入第一个整数");
+//        int j = sc.nextInt();
+//        System.out.println(j);
+
+
+//        System.out.println("请输入第一个字符串");
+//        String line1 = sc.nextLine();
+//        System.out.println("请输入第二个字符串");
+//        String line2 = sc.nextLine();
+
+//        System.out.println("整数");
+//        int i = sc.nextInt();
+//        System.out.println(i);
+//        System.out.println("字符");
+//        String s  =  sc.nextLine();
+//        System.out.println("s前面");
+//        System.out.println(s);
+//        System.out.println("s后面");
+
+        /* 原因:
+        *
+        * 空格和回车也被视作是字符串;
+        *
+        * */
+
+    }
+}
