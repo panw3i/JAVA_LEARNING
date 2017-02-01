fca17558f7289cc3b8c38c13650280fc3812eece
panw3i <panw3i@gmail.com>
2017-02-01 14:49:22 +0800

    大小写转换

--- src/com/baidu/String/Demo9.java
+++ src/com/baidu/String/Demo9.java
@@ -1,5 +1,7 @@
 package com.baidu.String;
 
+import com.baidu.bean.Person;
+
 /**
  * Created by pan on 2017/2/1.
  */
@@ -7,6 +9,30 @@ public class Demo9 {
     public static void main(String[] args) {
 //        test();
 //        demo2();
+//        demo7();
+//        test1();
+
+        //test2();
+
+
+
+
+    }
+
+    public static void test2() {
+        String s1 = "ABCDF";
+        String s2 = "abcdef";
+        System.out.println(s1.toLowerCase());
+        System.out.println(s2.toUpperCase());
+    }
+
+    public static void test1() {
+        Person p = new Person("张三",23);
+        String s = String.valueOf(p);
+        System.out.println(p);
+    }
+
+    public static void demo7() {
         char[] arr = {'a','b','c'};
         String s = String.valueOf(arr);  // 底层是由String类的构造方法完成的
         System.out.println(s);
