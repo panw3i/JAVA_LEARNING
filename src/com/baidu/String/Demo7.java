a985c3a521b6abd3815813b4ac9f20d1d32efcb2
panw3i <panw3i@gmail.com>
2017-02-01 13:58:55 +0800

    字符串的遍历

---
+++ src/com/baidu/String/Demo7.java
@@ -0,0 +1,15 @@
+package com.baidu.String;
+
+/**
+ * Created by pan on 2017/2/1.
+ */
+public class Demo7 {
+    public static void main(String[] args) {
+
+        String s =  "heima";
+
+        for (int i = 0; i < s.length(); i++) {
+            System.out.println(s.charAt(i));  // 参过索引返回每一个字符
+        }
+    }
+}
